c411fe1dee6b2f419f04ceeb29820fb16c6c85ae
Lukáš Utíkal <Luk Utkal>
2017-03-05 18:27:50 +0100

    add sins

--- src/services/sinsService.ts
+++ src/services/sinsService.ts
@@ -186,7 +186,264 @@ export class SinsService {
                         }
                     }
                 }
-            }
+            },
+            1: {
+                title: "sins_type_1",
+                questions: {
+                    0: {
+                        title: "sins_question_0",
+                        type: "multi",
+                        answers: {
+                            0: {
+                                title: "sins_question_0_answer_0",
+                                value: "1.2"
+                            },
+                            1: {
+                                title: "sins_question_0_answer_1",
+                                value: "1"
+                            },
+                            2: {
+                                title: "sins_question_0_answer_2",
+                                value: "1.5"
+                            },
+                            3: {
+                                title: "sins_question_0_answer_3",
+                                value: "1.1"
+                            },
+                            4: {
+                                title: "sins_question_0_answer_4",
+                                value: "2"
+                            },
+                            5: {
+                                title: "sins_question_0_answer_5",
+                                value: "3"
+                            },
+                        }
+                    }
+                },
+                1: {
+                    title: "sins_question_1",
+                    type: "base",
+                    answers: {
+                        0: {
+                            title: "sins_question_1_answer_0",
+                            value: "10"
+                        },
+                        1: {
+                            title: "sins_question_1_answer_1",
+                            value: "30"
+                        },
+                        2: {
+                            title: "sins_question_1_answer_2",
+                            value: "50"
+                        },
+                        3: {
+                            title: "sins_question_1_answer_3",
+                            value: "100"
+                        },
+                    }
+                },
+                2: {
+                    title: "sins_question_2",
+                    type: "multi",
+                    answers: {
+                        0: {
+                            title: "sins_question_2_answer_0",
+                            value: "1"
+                        },
+                        1: {
+                            title: "sins_question_2_answer_1",
+                            value: "2"
+                        },
+                        2: {
+                            title: "sins_question_2_answer_2",
+                            value: "1.5"
+                        },
+                        3: {
+                            title: "sins_question_2_answer_3",
+                            value: "2.5"
+                        },
+                    }
+                },
+                3: {
+                    title: "sins_question_3",
+                    type: "multi",
+                    answers: {
+                        0: {
+                            title: "sins_question_3_answer_0",
+                            value: "0.2"
+                        },
+                        1: {
+                            title: "sins_question_3_answer_1",
+                            value: "1"
+                        },
+                    }
+                }
+            },
+
+            2: {
+                title: "sins_type_2",
+                questions: {
+                    0: {
+                        title: "sins_question_0",
+                        type: "base",
+                        answers: {
+                            0: {
+                                title: "sins_question_0_answer_0",
+                                value: "30"
+                            },
+                            1: {
+                                title: "sins_question_0_answer_1",
+                                value: "60"
+                            },
+                            2: {
+                                title: "sins_question_0_answer_2",
+                                value: "100"
+                            },
+                        }
+                    },
+                    1: {
+                        title: "sins_question_1",
+                        type: "multi",
+                        answers: {
+                            0: {
+                                title: "sins_question_1_answer_0",
+                                value: "0.2"
+                            },
+                            1: {
+                                title: "sins_question_1_answer_1",
+                                value: "1"
+                            },
+                        }
+                    }
+                }
+            },
+            3: {
+                title: "sins_type_3",
+                questions: {
+                    0: {
+                        title: "sins_question_0",
+                        type: "base",
+                        answers: {
+                            0: {
+                                title: "sins_question_0_answer_0",
+                                value: "20"
+                            },
+                            1: {
+                                title: "sins_question_0_answer_1",
+                                value: "50"
+                            },
+                            2: {
+                                title: "sins_question_0_answer_2",
+                                value: "100"
+                            },
+                        }
+                    }
+                }
+            },
+            4: {
+                title: "sins_type_4",
+                questions: {
+                    0: {
+                        title: "sins_question_0",
+                        type: "base",
+                        answers: {
+                            0: {
+                                title: "sins_question_0_answer_0",
+                                value: "40"
+                            },
+                            1: {
+                                title: "sins_question_0_answer_1",
+                                value: "30"
+                            },
+                            2: {
+                                title: "sins_question_0_answer_2",
+                                value: "50"
+                            },
+                            3: {
+                                title: "sins_question_0_answer_3",
+                                value: "20"
+                            },
+                        }
+                    },
+                    1: {
+                        title: "sins_question_1",
+                        type: "multi",
+                        answers: {
+                            0: {
+                                title: "sins_question_1_answer_0",
+                                value: "1"
+                            },
+                            1: {
+                                title: "sins_question_1_answer_1",
+                                value: "0.5"
+                            },
+                        }
+                    }
+                }
+            },
+            5: {
+                title: "sins_type_5",
+                questions: {
+                    0: {
+                        title: "sins_question_0",
+                        type: "base",
+                        answers: {
+                            0: {
+                                title: "sins_question_0_answer_0",
+                                value: "30"
+                            },
+                            1: {
+                                title: "sins_question_0_answer_1",
+                                value: "60"
+                            },
+                            2: {
+                                title: "sins_question_0_answer_2",
+                                value: "30"
+                            },
+                            3: {
+                                title: "sins_question_0_answer_3",
+                                value: "40"
+                            },
+                            4: {
+                                title: "sins_question_0_answer_4",
+                                value: "50"
+                            },
+                            5: {
+                                title: "sins_question_0_answer_5",
+                                value: "60"
+                            },
+                            6: {
+                                title: "sins_question_0_answer_6",
+                                value: "80"
+                            },
+                            7: {
+                                title: "sins_question_0_answer_7",
+                                value: "100"
+                            },
+                        }
+                    },
+                    1: {
+                        title: "sins_question_1",
+                        type: "multi",
+                        answers: {
+                            0: {
+                                title: "sins_question_1_answer_0",
+                                value: "0.3"
+                            },
+                            1: {
+                                title: "sins_question_1_answer_1",
+                                value: "0.7"
+                            },
+                            2: {
+                                title: "sins_question_1_answer_2",
+                                value: "1"
+                            },
+                        }
+                    }
+                }
+            },
+
         };
         return Promise.resolve(sins[id]);
     }
